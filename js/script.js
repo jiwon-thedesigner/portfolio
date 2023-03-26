478fe980fba0efd13eedd1f7cff9e90e6077eb5d
jiwon-thedesigner <127606592+jiwon-thedesigner@users.noreply.github.com>
2023-03-26 17:47:26 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// side-bar mobile
+
+$('.side-bar-open').click(function(){
+    console.log('tlqkf');
+    $('.side-bar').addClass('active');
+});
+$('.side-bar-close').click(function(){
+    $('.side-bar').removeClass('active');
+});
